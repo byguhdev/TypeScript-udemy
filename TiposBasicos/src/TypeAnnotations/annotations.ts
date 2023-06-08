26cabeb1ae630c39bd281492f7f0aedaf583854b
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-06-07 23:54:29 -0300

    feat: :sparkles: Types Annotations

---
+++ TiposBasicos/src/TypeAnnotations/annotations.ts
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+// Tipos Básicos (aqui ocorre inferência de tipos)
+let nome: string = 'Guga'; // Qualquer tipo de string '' "" ``
+let idade: number = 30; // 10, 1.57, -5.55, 0xf00d, 0b1010, 0b7744
+let adulto: boolean = true; // true or false
+let simbolo: symbol = Symbol('Qualquer-symbol'); // symbol
+//let big: bigint = 10n;
+
+// Arrays
+
+let arraysNumbers: Array<number> = [1, 2, 3, 4, 5];
+let arraysNumbers2: number[] = [1, 2, 3, 4, 5];
+let arraysString: Array<string> = ['a', 'b', 'c'];
+let arraysString2: string[] = ['a', 'b', 'c'];
+
+
+// Objetos
+
