8040218e473b0393f005f4f0a35d0895a139c610
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-06-08 00:22:37 -0300

    feat: :sparkles: Type Annotations

--- TiposBasicos/src/TypeAnnotations/annotations.ts
+++ TiposBasicos/src/TypeAnnotations/annotations.ts
@@ -8,12 +8,40 @@ let simbolo: symbol = Symbol('Qualquer-symbol'); // symbol
 //let big: bigint = 10n;
 
 // Arrays
-
+//                 O tipo          O array em si.
 let arraysNumbers: Array<number> = [1, 2, 3, 4, 5];
 let arraysNumbers2: number[] = [1, 2, 3, 4, 5];
 let arraysString: Array<string> = ['a', 'b', 'c'];
 let arraysString2: string[] = ['a', 'b', 'c'];
 
-
 // Objetos
 
+let pessoas: {
+  nome: string;
+  sobrenome: string;
+  idade: number;
+  adulto?: boolean;
+  peso: number;
+} = {
+  nome: 'ADM',
+  sobrenome: 'Santos',
+  idade: 1000,
+  peso: 80,
+  adulto: true,
+};
+
+//console.log(pessoas);
+
+// Funções
+
+function multiplica(x: number, y: number): number {
+  return x * y;
+}
+//          O tipo                             A função em si.
+const soma: (x: number, y: number) => number = (x, y) => x + y;
+
+const multiplicar = multiplica(2, 66);
+const somar = soma(122, 22);
+
+console.log(multiplicar);
+console.log(somar);
