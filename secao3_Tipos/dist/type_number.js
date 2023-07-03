ee3694275b0a872e554401202fb8b00b20eed959
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-02 22:43:23 -0300

    Type Number

---
+++ secao3_Tipos/dist/type_number.js
@@ -0,0 +1,4 @@
+"use strict";
+let val1 = 10;
+let val2 = 10.212;
+console.log(val1 + val2);
---
+++ secao3_Tipos/src/type_number.ts
@@ -0,0 +1,5 @@
+// Type number (int, float, hex, binary)
+let val1: number = 10;
+let val2: number = 10.212;
+
+console.log(val1 + val2);
