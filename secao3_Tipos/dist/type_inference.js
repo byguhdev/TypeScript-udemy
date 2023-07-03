1918633f40f8d3b44e9ae324ba48111534b7df33
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-02 21:37:27 -0300

    Type Inference

---
+++ secao3_Tipos/dist/type_inference.js
@@ -0,0 +1,11 @@
+"use strict";
+let curso = "TypeScript";
+curso = "Novo Curso de TypeScript";
+let tecs = [
+    "PHP",
+    "JS",
+    "TS",
+    "React",
+    2023,
+];
+console.log(curso);
---
+++ secao3_Tipos/src/type_inference.ts
@@ -0,0 +1,18 @@
+let curso = "TypeScript";
+
+/*O tipo 'number' não pode ser atribuído ao tipo 'string'. 
+curso = 202;
+*/
+
+curso = "Novo Curso de TypeScript";
+
+// let tecs: (string | number)[]
+let tecs = [
+    "PHP",
+    "JS",
+    "TS",
+    "React",
+    2023,
+]
+
+console.log(curso);
