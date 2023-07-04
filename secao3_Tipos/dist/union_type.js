f146beec3e728547de45eb3fdb5b04abf6814a35
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-03 21:43:57 -0300

    Union Type

---
+++ secao3_Tipos/dist/union_type.js
@@ -0,0 +1,4 @@
+"use strict";
+let userId;
+userId = 12;
+console.log(typeof userId);
---
+++ secao3_Tipos/src/union_type.ts
@@ -0,0 +1,7 @@
+// Union Type --> União de tipos.
+let userId: number | string;
+
+userId = 12; // number
+//userId = "12"; // string
+
+console.log(typeof userId);
