d3e6c866e9974e81a684cfea0800e2975323d238
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-03 22:11:58 -0300

    Type Boolean

---
+++ secao3_Tipos/dist/type_boolean.js
@@ -0,0 +1,5 @@
+"use strict";
+let auth = true;
+let codeStatus = '1';
+auth = Boolean(codeStatus);
+console.log(auth);
---
+++ secao3_Tipos/src/type_boolean.ts
@@ -0,0 +1,10 @@
+// Type BOOLEAN
+
+let auth: boolean = true;
+
+let codeStatus: string = '1';
+
+// tudo que for diferente de zero, string vazia, undefined, será verdadeiro.
+auth = Boolean(codeStatus);
+
+console.log(auth);
