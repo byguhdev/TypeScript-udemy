44f9144490bd5519d0a60d81c5e52e39bc886c46
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-04 23:52:30 -0300

    Type Tuplas

---
+++ secao3_Tipos/dist/type_tupla.js
@@ -0,0 +1,5 @@
+"use strict";
+let aluno;
+aluno = ["Henrique", 0];
+aluno.push("Math", 1);
+console.log(aluno);
---
+++ secao3_Tipos/src/type_tupla.ts
@@ -0,0 +1,8 @@
+// Tupla não possui no JS e sim apenas com TS
+
+let aluno: [string, number]; // precisa seguir a mesma ordem passada.
+
+aluno = ["Henrique", 0]; // Só pérmite adicionar 2 elementos
+aluno.push("Math", 1); // Mais eu posso adicionar mais com o push.
+
+console.log(aluno);
