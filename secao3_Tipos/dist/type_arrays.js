0a8552f99dd2343f50093d7968ad691cd61bf38b
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-04 23:24:50 -0300

    Type Arrays

---
+++ secao3_Tipos/dist/type_arrays.js
@@ -0,0 +1,5 @@
+"use strict";
+let filmes;
+filmes = ["Filme 1", "Filme 2", "...", 2023];
+filmes.push(12);
+console.log(filmes);
---
+++ secao3_Tipos/src/type_arrays.ts
@@ -0,0 +1,11 @@
+// Type Array
+
+//let filmes: Array<string>;
+//let filmes: string[];
+//let filmes: Array<string | number>;
+let filmes: (string | number)[];
+
+filmes = ["Filme 1", "Filme 2", "...", 2023]
+
+filmes.push(12);
+console.log(filmes);
