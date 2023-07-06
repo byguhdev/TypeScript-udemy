27ac43a38fa225e2994ad3eb52ec66d6dced9bb4
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-05 22:34:17 -0300

    Type Object

---
+++ secao3_Tipos/dist/type_object.js
@@ -0,0 +1,12 @@
+"use strict";
+let newUser;
+newUser = {
+    nome: "Henrique",
+    sobrenome: "Santos",
+    idade: 17.8,
+};
+let podeSerAssim = {
+    title: "O objeto pode ser assim..."
+};
+console.log(newUser);
+console.log(podeSerAssim);
---
+++ secao3_Tipos/src/type_object.ts
@@ -0,0 +1,16 @@
+// Type Object
+
+let newUser: object;
+
+newUser = {
+  nome: "Henrique",
+  sobrenome: "Santos",
+  idade: 17.8,
+};
+
+let podeSerAssim: object = {
+    title: "O objeto pode ser assim..."
+};
+
+console.log(newUser);
+console.log(podeSerAssim);
