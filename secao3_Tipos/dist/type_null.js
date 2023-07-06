03de81fb58bad71b0e12c3cc5d0724ae80662653
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-05 23:09:47 -0300

    Type null

---
+++ secao3_Tipos/dist/type_null.js
@@ -0,0 +1,7 @@
+"use strict";
+let nome;
+nome = "Dr NULL";
+nome = null;
+console.log(nome);
+let indefinido;
+console.log(indefinido);
---
+++ secao3_Tipos/src/type_null.ts
@@ -0,0 +1,18 @@
+// Type null
+
+/*
+    1 - "null" é um valor atribuído explicitamente por um programador para indicar a ausência intencional de um objeto ou valor.
+
+    2 - "undefined", por outro lado, é um valor atribuído automaticamente a uma variável que não possui um valor atribuído ou a uma propriedade que não foi definida em um objeto.
+*/
+
+// Usando o null
+let nome: string | null;
+nome = "Dr NULL";
+nome = null;
+console.log(nome);
+
+// Usando o undefined
+// undefined -> padrão de toda variavel quando não se sabe sua tipagem.
+let indefinido;
+console.log(indefinido);
