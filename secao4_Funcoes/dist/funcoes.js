9133d3b57e8fc12cdecdb78c797625a570756b64
Gustavo Henrique <gustavobarbosaafc2019@gmail.com>
2023-07-07 22:45:48 -0300

    Funções

---
+++ secao4_Funcoes/dist/funcoes.js
@@ -0,0 +1,13 @@
+"use strict";
+let n1 = 10;
+let n2 = 23;
+function soma(num1, num2) {
+    let somar = num1 + num2;
+    return somar;
+}
+console.log(soma(n1, n2));
+function mostraCaracteres(varchar) {
+    return varchar;
+}
+console.log(mostraCaracteres(112));
+console.log(mostraCaracteres("@&$<>"));
